e31c423d0c54366cf043b32d42fc1d040d199ce3
Andrew Levine <DrewML@users.noreply.github.com>
2018-12-21 16:21:10 -0600

    Add php/xml files to scripts/licenseHeaderCheck.js (#50)
    ## This PR is a:
    
    - [ ] New feature
    - [ ] Enhancement/Optimization
    - [ ] Refactor
    - [X] Bugfix
    - [ ] Test for existing code
    - [ ] Documentation

--- Magento_BundleConfig/Plugin/View/Asset/MinificationPlugin.php
+++ Magento_BundleConfig/Plugin/View/Asset/MinificationPlugin.php
@@ -1,4 +1,8 @@
 <?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
 namespace Magento\BundleConfig\Plugin\View\Asset;
 
 use Magento\Framework\View\Asset\Minification;
