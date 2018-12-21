5ae1e701bf865c0eb78db76564e705ef14d39108
Lachlan Turner <40189797+aligent-lturner@users.noreply.github.com>
2018-12-22 06:21:25 +1030

    Make code PSR-2 compliant

--- Magento_BundleConfig/Plugin/View/Asset/MinificationPlugin.php
+++ Magento_BundleConfig/Plugin/View/Asset/MinificationPlugin.php
@@ -1,6 +1,8 @@
 <?php
 namespace Magento\BundleConfig\Plugin\View\Asset;
 
+use Magento\Framework\View\Asset\Minification;
+
 /**
  * Class MinificationPlugin
  * @package Magento\BundleConfig\Plugin\View\Asset
@@ -10,15 +12,16 @@ class MinificationPlugin
     /**
      * Minfication should be disabled whenever BundleConfig module is active
      *
-     * @param \Magento\Framework\View\Asset\Minification $subject
+     * @param Minification $subject
      * @param \Closure $proceed
      * @param string $contentType
      * @return bool
      */
     public function aroundIsEnabled(
-        \Magento\Framework\View\Asset\Minification $subject,
+        Minification $subject,
         \Closure $proceed,
-        $contentType) {
+        $contentType
+    ) {
         // always disable minification for JS
         if ($contentType === 'js') {
             return false;
